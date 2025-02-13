dd213e690a1b65a33a8258afb46f54b64dc9c54e
Shaneshwar Bhagat <49476737+Shaneshwarbhagat@users.noreply.github.com>
2025-02-13 19:14:53 +0530

    trying this now

--- src/api/json-server.js
+++ src/api/json-server.js
@@ -1,8 +1,14 @@
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(__dirname + "/db.json"); // Ensure db.json is in the same directory
+const router = jsonServer.router("./api/db.json"); // Use relative path
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
 
 module.exports = server;
