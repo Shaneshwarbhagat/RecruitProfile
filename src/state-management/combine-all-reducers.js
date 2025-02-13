9c5f4be717658923499a06c32c440154cfdfd0b8
Shaneshwar Bhagat <49476737+Shaneshwarbhagat@users.noreply.github.com>
2025-02-13 18:07:50 +0530

    js to ts

--- src/state-management/combine-all-reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import profileReducer from './reducer';
-
-const rootReducer = combineReducers({
-  profile: profileReducer
-});
-
-export default rootReducer;
