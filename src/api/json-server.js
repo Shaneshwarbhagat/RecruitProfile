772db1af53aa5b165018236213d6a72280353cc0
Shaneshwar Bhagat <49476737+Shaneshwarbhagat@users.noreply.github.com>
2025-02-13 19:19:39 +0530

    build fix

--- src/api/json-server.js
+++ src/api/json-server.js
@@ -1,14 +1,14 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import serverless from "serverless-http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = jsonServer.create();
-const router = jsonServer.router("./api/db.json"); // Use relative path
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
-
-module.exports = server;
+export const handler = serverless(server);
